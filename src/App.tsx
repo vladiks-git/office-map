2a915dcba64b53400e48e2529ce5bff704dd30a6
vershov <vershov@clink.tech>
2022-04-17 16:56:17 +0400

    add hover animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { ReactSVG } from 'react-svg';
 import './App.css';
 
 function App() {
+
+  const handleClick = (e: any) => {
+    console.log(e.target.closest('.hov'));
+  };
+
   return (
-    <div className="App">
-      its worked
+    <div className='App'>
+      <div onClick={handleClick}>
+        <ReactSVG src='full.svg' />
+      </div>
     </div>
   );
 }
