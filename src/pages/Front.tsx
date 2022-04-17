3de049c923ba0345a8d077d243b8bc6253f06823
vershov <vershov@clink.tech>
2022-04-17 18:48:03 +0400

    add styles and ids for svg pc

--- src/pages/Front.tsx
+++ src/pages/Front.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { ReactSVG } from 'react-svg';
 
 const Front = () => {
   return (
     <div>
       front
+      <div onClick={e => console.log(e.target)}>
+        <ReactSVG src='section.svg' />
+      </div>
     </div>
   );
 };
